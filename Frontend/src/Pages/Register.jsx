857d11c43225126fd45dbf0a68797dfaabc92040
Ratikanta Behera <ratikantabehera360@gmail.com>
2024-10-25 20:10:15 +0530

    change_in_frontend

--- Frontend/src/Pages/Register.jsx
+++ Frontend/src/Pages/Register.jsx
@@ -23,7 +23,7 @@ const Register = () => {
     try {
       await axios
         .post(
-          "http://localhost:4000/api/v1/user/patient/register",
+          "https://hospital-management-system-5-hwqd.onrender.com/api/v1/user/patient/register",
           { firstName, lastName, email, phone, nic, dob, gender, password },
           {
             withCredentials: true,
@@ -93,8 +93,8 @@ const Register = () => {
           <div>
             <input
               type="number"
-              placeholder="NIC"
-              value={nic}
+              placeholder="UIDI"
+              value={uidi}
               onChange={(e) => setNic(e.target.value)}
             />
             <input
