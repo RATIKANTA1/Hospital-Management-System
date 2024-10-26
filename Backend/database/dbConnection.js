461c0269fc82468e50803cb770c79eca6002f6ce
Ratikanta Behera <ratikantabehera360@gmail.com>
2024-10-27 00:52:52 +0530

    update_backend

--- Backend/database/dbConnection.js
+++ Backend/database/dbConnection.js
@@ -1,13 +1,14 @@
 import mongoose from "mongoose";
 
-export const dbConnection = ()=>{
-    mongoose.connect(process.env.MONGO_URI,{
-        dbName: "HOSPITAL_MANAGEMENT_SYSTEM"
+export const dbConnection = () => {
+  mongoose
+    .connect(process.env.MONGO_URI, {
+      dbName: "MERN_STACK_HOSPITAL_MANAGEMENT_SYSTEM",
     })
     .then(() => {
-        console.log("Connected to database!");
+      console.log("Connected to database!");
     })
-    .catch((err) =>{
-        console.log(`Some error occured while connecting to database: ${err}`);
+    .catch((err) => {
+      console.log("Some error occured while connecting to database:", err);
     });
-}
+};
