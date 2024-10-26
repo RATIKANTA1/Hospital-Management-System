7c0e774a9e6abba717c496ceab18aba8e9f09066
Ratikanta Behera <ratikantabehera360@gmail.com>
2024-10-26 23:15:30 +0530

    error_solve

--- Backend/router/appointmentRouter.js
+++ Backend/router/appointmentRouter.js
@@ -12,13 +12,9 @@ import {
 
 const router = express.Router();
 
-
 router.post("/post", isPatientAuthenticated, postAppointment);
-
 router.get("/getall", isAdminAuthenticated, getAllAppointments);
-
 router.put("/update/:id", isAdminAuthenticated, updateAppointmentStatus);
-
 router.delete("/delete/:id", isAdminAuthenticated, deleteAppointment);
 
 export default router;
